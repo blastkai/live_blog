9676c283f8044427793839fa2ce28b28141f20e3
Kai <Kai>
2015-05-13 04:41:18 +0800

    post delete fix

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # devise_for :users, ActiveAdmin::Devise.config
   # devise_for :admin_users, ActiveAdmin::Devise.config
   
-  resources :posts ,:blogcomments
+  resources :posts, :blogcomments
   post 'blogcomments/createcomment' => 'blogcomments#createcomment'
   root "posts#index"
 =begin
